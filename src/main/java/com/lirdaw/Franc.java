7e429b76c1269f25390659edbf21716d61fac5af
lirdaw <lirdaw@gmail.com>
2020-03-16 20:13:51 +0100

    Franc currency added

---
+++ src/main/java/com/lirdaw/Franc.java
@@ -0,0 +1,19 @@
+package com.lirdaw;
+
+class Franc {
+    private int amount;
+
+    Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        Franc franc = (Franc) object;
+        return this.amount == franc.amount;
+    }
+}
--- src/test/java/com/lirdaw/MoneyTest.java
+++ src/test/java/com/lirdaw/MoneyTest.java
@@ -17,4 +17,18 @@ class MoneyTest {
         Assertions.assertEquals(new Dollar(5), new Dollar(5));
         Assertions.assertNotEquals(new Dollar(5), new Dollar(6));
     }
+
+    @Test
+    void testFrancMultiplication() {
+        Franc five = new Franc(5);
+
+        Assertions.assertEquals(new Franc(10), five.times(2));
+        Assertions.assertEquals(new Franc(15), five.times(3));
+    }
+
+    @Test
+    void testFrancEquality() {
+        Assertions.assertEquals(new Franc(5), new Franc(5));
+        Assertions.assertNotEquals(new Franc(5), new Franc(6));
+    }
 }
